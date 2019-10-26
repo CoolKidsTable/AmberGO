fb7756b37fe0d6da9cd804d0fd55179777ca39cb
Strassboom <maddog241996@gmail.com>
2019-10-26 19:37:20 -0400

    Began adapting webapp for gas station location purposes.

--- app.py
+++ app.py
@@ -12,10 +12,21 @@ def wanttohelp():
     if request.method == "GET":
         return render_template("assistpage.html")
         
-@app.route('/',methods = ["GET","POST"])
+@app.route('/starttalking',methods = ["GET","POST"])
 def index():
     if request.method == "GET":    
         return render_template("input.html")
+    elif request.method == "POST":
+        if request.form["selectinput"] == "I need help":
+            return redirect(url_for("needhelp"))
+        else:
+            return redirect(url_for("wanttohelp"))
+        return render_template("input2.html",name=request.form["tempusername"])
+
+@app.route('/',methods = ["GET","POST"])
+def report():
+    if request.method == "GET":    
+        return render_template("reportpage.html")
     elif request.method == "POST":
         if request.form["selectinput"] == "I need help":
             return redirect(url_for("needhelp"))
